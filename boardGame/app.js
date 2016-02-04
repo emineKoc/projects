4d88cdfd4839832e715ce31fea0ddf3c2899a4a4
emineKoc <eminekoc@gmail.com>
2016-02-04 13:22:51 -0500

    I created the turns. Now 2 player plays

--- boardGame/app.js
+++ boardGame/app.js
@@ -1,24 +1,59 @@
 $(document).ready(function() {
 
-// rollThedice()
 
 $('button').on('click', function(){
-    $('#step1').append( $('#player1') )    /// i should change the player to current player with function
+    var  $message = $('#message')
+    $message.slideToggle();
+    $message.html('Please choose your Player');
 });
 
-$('p').on('click', function() {
-move()
+choosePlayer();
 
-});
+  $('div').find('#rollDice').on('click', chooseandmove ) ;
 
-
-
+/// i should change the player to current player with function
 
 
-});
+
+});   // Onload function ends here.
 
 
-//I ll use to make a rolling effect.
+
+var currentPlayer = '';
+function choosePlayer(){
+
+      $('#player1').on('click', function(){
+          currentPlayer = '#player1';
+          $('#step1').append($(currentPlayer));
+       return console.log('player 1 has chosen');
+     }) ;
+
+      $('#player2').on('click', function(){
+          currentPlayer = '#player2';
+          $('#step1').append($(currentPlayer));
+          return console.log('player 2 has chosen');
+
+      })
+}
+
+
+function chooseandmove() {
+
+  if (currentPlayer == "#player1") {
+    player1.move() ;
+    currentPlayer = "#player2" ;
+  }else if (currentPlayer == "#player2") {
+   player2.move();
+   currentPlayer = "#player1" ;
+
+  };
+
+}
+
+
+
+//Here I m creating an object literal.// I ll use to make a rolling effect.
+
 var dice = {
   1 : './img/dice/1.gif',
   2 : './img/dice/2.gif',
@@ -28,26 +63,47 @@ var dice = {
   6 : './img/dice/6.gif',
 }
 
-
-// for (var i  = 1; i < 7 ; i++) {
-//      $('img').find('#dice').attr('src', dice[i])
-// }
-
-
 var numberOfSteps;
-function rollThedice()  {
-    numberOfSteps =  Math.floor(Math.random() *6+ 1 )
+rollThedice = function ()  {
+    numberOfSteps =  Math.floor(Math.random() * 6 +1);
     $('#dice').attr('src', dice[numberOfSteps] );
+    console.log(numberOfSteps)
     return numberOfSteps
+};
+
+
+
+
+// create an CONTRACTOR FUCNTION FOR EACH PLAYER
+
+var Player = function (player) {
+
+this.name = player;
+this.currentStep = 1;
+this.currentTile = '#step0';
+
+
+ this.move = function ( ){
+ // I need the result from the dice.  // I need the corrent place of the player
+ var movingsteps = rollThedice();
+ this.currentStep += movingsteps ;
+ this.currentTile = '#step' + this.currentStep.toString();
+ $(this.currentTile).append( $( this.name ) ) ;
+ };
+
+ this.nextTurn = function () {
+   if (this.turn = false) {
+     this.turn = true;
+   }else {
+     this.turn= false;
+   }
+ };
+
+
+
 }
 
-var currentStep = 1;
 
-function move() {
-// I need the result from the dice.
-// I need the corrent place of the player
-currentStep += rollThedice();
-var currentTile = '#step' + currentStep.toString();
-$(currentTile).append( $('#player1') ) ;
 
-}
+var player1 =  new Player('#player1');
+var player2 = new Player('#player2');
