b4c0554afb1ba62cdde6591028e4bfd232ab81f7
emineKoc <eminekoc@gmail.com>
2016-02-05 12:27:24 -0500

    I m adding prices and traps

--- boardGame/app.js
+++ boardGame/app.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
+$('#message').slideToggle();
 
-
-$('button').on('click',function(){
-    var  $message = $('#message')
-    $message.slideToggle();
-    $message.html('Each Player ');
-});
+  $('button').on('click',function(){
+  $('#message').slideToggle();
+  });
 
 choosePlayer();
-$('div').find('#rollDice').on('click', chooseAndMove ) ;
 
-/// i should change the player to current player with function
+$('div').find('#rollDice').on('click', chooseAndMove ) ;
 
 
 });   // Onload function ends here.
@@ -39,12 +36,13 @@ function chooseAndMove() {
 
   if (currentPlayer == "#player1") {
     player1.move() ;
+    checkSteps(player1);
     currentPlayer = "#player2" ;
-    checkSteps();
+
   }else if (currentPlayer == "#player2") {
    player2.move();
+   checkSteps(player2);
    currentPlayer = "#player1" ;
-   checkSteps();
 
   };
 
@@ -82,7 +80,6 @@ this.name = player;
 this.currentStep = 1;
 this.currentTile = '#step0';
 
-
  this.move = function ( ){
  // I need the result from the dice.  // I need the corrent place of the player
  var movingsteps = rollThedice();
@@ -91,19 +88,20 @@ this.currentTile = '#step0';
  $(this.currentTile).append( $( this.name ) ) ;
  };
 
- this.nextTurn = function () {
-   if (this.turn = false) {
-     this.turn = true;
-   }else {
-     this.turn= false;
-   }
- };
-
 
 
 }
 
 
+function price(x){
+  x.currentStep += 2;
+  x.currentTile = '#step' + x.currentStep.toString();
+  $(x.currentTile).append( $( x.name ) ) ;
+}
+
+function () {
+
+}
 
 
 
@@ -112,86 +110,95 @@ var player2 = new Player('#player2');
 
 
 ///Challenges:
-function checkSteps() {
+function checkSteps(playerx) {
 
-switch ( player1.currentTile || player2.currentTile ) {
+//   currentPlayer  can ve "#player1" or "#player2"
 
-case "#step3"   :
-$('#challen1').css('display', 'block');
+  switch ( playerx.currentTile ) {
 
-$('input').on('keydown',function (event) {
-  if (event.keyCode == 13) {
-    console.log('hit enter!' + player1.currentStep ||  player2.currentStep);
-    var answer = $('#challenge1Answer').val()
-    if (answer == '3'){
-      console.log('true');
-      $('#challen1').css('display', 'none');
-    } else {
-      console.log('false')
-      $('#challen1').css('display', 'none');
-    }
-  }
-});
+    case "#step3"   :
+    $('#challen1').css('display', 'block');
 
-break;
+    $('input').on('keydown',function (event) {
+      if (event.keyCode == 13) {
+        console.log('hit enter!');
+        var answer = $('#challenge1Answer').val()
+        if (answer == '3'){
+          console.log('true');
+          $('#challen1').css('display', 'none');
 
-case "#step13"   :
-$('#challen2').css('display', 'block');
+          playerx.currentStep += 2;
+          playerx.currentTile = '#step' + playerx.currentStep.toString();
+          $(playerx.currentTile).append( $( playerx.name ) ) ;
+        } else {
+          console.log('false')
+          $('#challen1').css('display', 'none');
+          playerx.currentStep -= 2;
+          playerx.currentTile = '#step' + playerx.currentStep.toString();
+          $(playerx.currentTile).append( $( playerx.name ) ) ;
+        }
+      }
+    });
 
+    break;
 
-$('input').on('keydown',function (event) {
-  if (event.keyCode == 13) {
-    console.log('hit enter!'+ player1.currentStep ||  player2.currentStep);
-    var answer1 = $('#challenge2Answer').val()
-    if (answer1 == 'closure'){
-      console.log('true')
-      $('#challen2').css('display', 'none');
-    } else {
-      console.log('false')
-      $('#challen2').css('display', 'none');
-    }
-  }
-});
-
-break;
-
-case "#step20"   :
-$('#challen3').css('display', 'block');
+    case "#step13"   :
+    $('#challen2').css('display', 'block');
 
 
-$('input').on('keydown',function (event) {
-  if (event.keyCode == 13) {
-    console.log('hit enter!'+ player1.currentStep ||  player2.currentStep );
-    var answer2 = $('#challenge3Answer').val  ()
-    if (answer2 == '122' || "122"){
-      console.log('true');
-      $('#challen3').css('display', 'none');
-    } else {
-      console.log('false');
-      $('#challen3').css('display', 'none');
-    }
-  }
-});
+    $('input').on('keydown',function (event) {
+      if (event.keyCode == 13) {
+        console.log('hit enter!'+ playerx.currentStep );
+        var answer1 = $('#challenge2Answer').val()
+        if (answer1 == 'closure'){
+          console.log('true')
+          $('#challen2').css('display', 'none');
+        } else {
+          console.log('false')
+          $('#challen2').css('display', 'none');
+        }
+      }
+    });
 
-break;
+    break;
+
+    case "#step20"   :
+    $('#challen3').css('display', 'block');
+
+    $('input').on('keydown',function (event) {
+      if (event.keyCode == 13) {
+        console.log('hit enter!');
+        var answer2 = $('#challenge3Answer').val  ()
+        if (answer2 == '122' || "122"){
+          console.log('true');
+          $('#challen3').css('display', 'none');
+
+        } else {
+          console.log('false');
+          $('#challen3').css('display', 'none');
+        }
+      }
+    });
+
+    break;
+
+
+    case "#step11"   :   //IMPLEMENT THE HANGMAN
+
+    break;
+
+
+    case "#step8"   :   //LUCKY TILE
+
+
+    break;
 
 
 
-case "#step11"   :   //IMPLEMENT THE HANGMAN
-
-break;
+    case "#step6"   :    //PICK A AGAME
 
 
-case "#step8"   :   //LUCKY TILE
-
-break;
-
-
-
-case "#step6"   :    //PICK A AGAME
-
-
-break;
+    break;
 
 
 case "#step18"   : // PICK A GAME
